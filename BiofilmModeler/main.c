96438a0c6b06c720fd185f030e9a399c9a59962f
Brian Thomas Ho <brianthomasho@icloud.com>
2018-12-29 22:59:35 -0600

    Updating main.c

--- BiofilmModeler/main.c
+++ BiofilmModeler/main.c
@@ -2,14 +2,47 @@
 //  main.c
 //  BiofilmModeler
 //
-//  Created by Brian Ho on 12/29/18.
+//  Created by Mekalanos Lab on 12/6/18.
 //  Copyright © 2018 Brian Thomas Ho. All rights reserved.
 //
 
-#include <stdio.h>
+
+#include <stdio.h>     // input/outputs
+#include <time.h>      // for seeding RNG
+#include <stdlib.h>    // memory management
+#include "BiofilmModeler.h"
+#include "ArgumentHandler.h"
+#include "ErrorHandler.h"
+#include "InitializeMatrix.h"
+#include "ComputeMatrix.h"
+#include "PrintMatrix.h"
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  
+  // variable declaration
+  int i;
+  settings_t settings;
+  matrix_t biofilm;
+  
+  // read input arguments
+  if (NULL == arguments_handler(&settings, argc, argv))       // define values of settings
+    error_handler('A', "Error reading arguments");
+  
+  
+  // initialize environment
+  srand((unsigned int) time(NULL));                           // seed RNG with system time
+  if (NULL == initialize_matrix(&biofilm, &settings))         // initialize matrix
+    error_handler('I', "Error initializing matrix\n");
+  
+  print_matrix(&biofilm, &settings);
+  census_matrix(&biofilm, &settings);
+  
+  for (i = 0; i < settings.iterations; i++) {
+    update_matrix(&biofilm, &settings);
+    print_matrix(&biofilm, &settings);
+    print_para(&biofilm, &settings);
+    census_matrix(&biofilm, &settings);
+  }
+  
   return 0;
 }
